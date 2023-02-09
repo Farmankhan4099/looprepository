b4d2ed7bcc61b367b16f64cb2546449e97d4a7bb
unknown <farmankhan874099@gmail.com>
2023-02-09 14:23:38 +0530

    new loop file added

---
+++ script.js
@@ -0,0 +1,109 @@
+//  ans-1
+
+let a = 16;
+let b = 12;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// ans-2
+
+c = 14;
+if (c % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+
+// ans-3
+
+// let name = prompt("enter your name");
+// alert(name);
+
+// ans-4
+
+let e = 12;
+let f = 12;
+let myName = "farman";
+let myAge = 21;
+let myNumbar = 916934980;
+let myFatherName = "ayyub";
+let myFatherAge = 46;
+let myFatherNumbar = 9602603004;
+
+if (e < f) {
+  console.log(myName);
+  console.log(myAge);
+  console.log(myNumbar);
+} else {
+  console.log(myFatherName);
+  console.log(myFatherAge);
+  console.log(myFatherNumbar);
+}
+
+// ans-10
+
+let p = 0;
+console.log(++p);
+console.log(++p);
+console.log(++p);
+console.log(++p);
+console.log(++p);
+console.log(++p);
+console.log(++p);
+console.log(++p);
+console.log(++p);
+console.log(++p);
+
+let q = 11;
+console.log(--q);
+console.log(--q);
+console.log(--q);
+console.log(--q);
+console.log(--q);
+console.log(--q);
+
+// ans-6
+// window.alert("hello world");
+// console.log("hello world");
+// document.write("hello world");
+
+// ans-8
+
+// let question = confirm("Do you want to play");
+// if (question) {
+//   alert("yes");
+// } else {
+//   alert("no");
+// }
+
+// ans-9
+
+let members = 4;
+if (members > 5) {
+  console.log("big family");
+} else if (members > 3 && members < 5) {
+  console.log("small family");
+} else {
+  console.log("very small family");
+}
+
+// ans-7
+
+let ab = 0;
+if (ab > 1) {
+  console.log("true");
+} else {
+  console.log("falsy");
+}
+
+// ans-5
+
+let x = 20;
+let y = 30;
+if (x % 5 === 0 && x % 10 === 0 && y % 5 === 0 && y % 10 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
